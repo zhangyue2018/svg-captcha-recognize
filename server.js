ca32ca36554587acc9f4a088c521fd7f9734a7f8
ZY <1822545638@qq.com>
2024-02-19 22:17:08 +0800

    feat#增加提示

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const { recognize } = require('./index')
 const port = 8888;
 
 const server = ws.createServer( connect => {
-    console.log('====新的连接====');
     connect.on('text', data => {
         try {
             let jsonData = JSON.parse(data);
             let code = recognize(jsonData.svg);
-            console.log('识别结果：', code);
+            let dateTime = new Date().toLocaleString();
+            console.log('time:', dateTime, '---识别结果：', code);
             connect.sendText(code);
         } catch(e) {
             console.log('识别code出错--', e);
